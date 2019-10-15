d9d9ee863d865ac66650e71264c8692ca503bee3
Dilum Bandara <33807852+dilumb@users.noreply.github.com>
2019-10-15 20:51:11 +1100

    Add files via upload
    Replacing PyPDF2 with PDFMiner.6

--- PDF_Handler.py
+++ PDF_Handler.py
@@ -1,45 +1,36 @@
 '''
 Created on Oct 7, 2018
+Updated on Oct 15, 2019
 
-@author: DilumBandara
+@author: Dilum Bandara
+Replacing PyPDF2 with PDFMiner
 '''
 
-import PyPDF2
+#import PyPDF2
+import pdfminer.layout
+import pdfminer.high_level
+import io
 
 class PDF_Handler(object):
     '''
     classdocs
     '''
 
-
     def __init__(self, file_name):
         '''
         Constructor
         '''
         self.file_name = file_name
-        self.pdf_reader = PyPDF2.PdfFileReader(self.file_name)
-        
-    def get_text(self, page_no):
-        '''
-        Get text on given page
-        '''
-        page = self.pdf_reader.getPage(page_no)
-        return page.extractText()
 
 
     def get_all_text(self):
         '''
         Get text on all pages
-        '''        
-        full_text = ''
-        for i in range(self.pdf_reader.getNumPages()):
-            page = self.pdf_reader.getPage(i)
-            full_text += page.extractText() + "\n"
-        return full_text
-    
-    
-    def get_no_pages(self):
         '''
-        Get no of pages in document
-        '''
-        return self.pdf_reader.getNumPages()
+        output = io.StringIO()
+        laparams = pdfminer.layout.LAParams()
+
+        with open(self.file_name, "rb") as pdffile:
+            pdfminer.high_level.extract_text_to_fp(pdffile, output, laparams=laparams)
+
+        return output.getvalue()
--- Typos.py
+++ Typos.py
@@ -1,7 +1,7 @@
 '''
 Created on Oct 7, 2018
 
-@author: DilumBandara
+@author: Dilum Bandara
 '''
 
 import sys
@@ -27,23 +27,16 @@ if __name__ == '__main__':
     
     if file_type == 'PDF':
         ph = PDF_Handler(sys.argv[2])
-        page_nos = ph.get_no_pages()
-        print('Succesfully open PDF file with', page_nos, ' pages.')
-        print("Searching for typos...")
-        for i in range(page_nos):
-            doc_text = ph.get_text(i)
-
-            
-        #break
+        doc_text = ph.get_all_text()
+        print('Succesfully open PDF file.')
     elif file_type == 'Word':
         wd = Word_Handler(sys.argv[2])
         doc_text = wd.get_all_text()
         print('Succesfully open Word file')
-        print("Searching for typos...")
     else:
         print('Unknown file type. Only PDF and Word are supported')
         sys.exit()
-    
+    print("Searching for typos...")
     st.scan_all(doc_text, -1)
     path = os.path.dirname(os.path.abspath(sys.argv[2]))
     st.dump_all(path + '\\' + dump_file)
@@ -51,4 +44,3 @@ if __name__ == '__main__':
     if st.typos_found:
         print("Typos found!")  
     print("Searching for typos complete.")
-           
